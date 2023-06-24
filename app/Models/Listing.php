cce63778ca9763650dec9902062d46019fc4feaf
Cédric <work@cedricverlinden.com>
2023-06-25 01:47:21 +0200

    Finished CRUD operations on listings

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Listing extends Model {
     use HasFactory;
 
+    protected $fillable = ['title', 'logo', 'company', 'location', 'website', 'email', 'tags', 'description'];
+
     public function scopeFilter($query, array $filters) {
         if ($filters['tag'] ?? false) {
             $query->where('tags', 'like', '%' . request('tag') . '%');
